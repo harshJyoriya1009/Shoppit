2f3d3009eae1f41f53c9fad44758840fd91df9c6
Harsh Jyoriya <harshjyoriyagdg24@gmail.com>
2025-09-07 16:55:52 +0530

    deployment

--- shoppit/settings.py
+++ shoppit/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure--=r5fv#!x6-$7w9+^xfz$ff2xiht5j$%=gy51@t8g(+oca0)m1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["jyoriya-store-ljs6.onrender.com", "localhost" , "127.0.0.1"]
 
 
 # Application definition
